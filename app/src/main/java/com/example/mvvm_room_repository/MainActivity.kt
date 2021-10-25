d78151d339738cbe99d186a81a15eaf8aac4f668
hunderboy <masury2011@gmail.com>
2021-10-25 18:17:56 +0900

    데이터 바인딩 OK

--- app/src/main/java/com/example/mvvm_room_repository/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_room_repository/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.mvvm_room_repository
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.mvvm_room_repository.databinding.ActivityMainBinding
+import com.example.mvvm_room_repository.room.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        /**
+         * ViewModel은 공유 가능합니다. 만약 activity의 ViewModel을 Fragment에서도 쓰고 싶다면, 아래와 같이 사용할 수 있습니다.
+         */
+
     }
+
+
 }
--- app/src/main/java/com/example/mvvm_room_repository/room/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/mvvm_room_repository/room/viewmodel/MainViewModel.kt
@@ -1,13 +1,21 @@
 package com.example.mvvm_room_repository.room.viewmodel
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val _main_text : MutableLiveData<String> = MutableLiveData()
-    val rightLegDegreeLiveData: LiveData<String> get() = _main_text
+    val mApplication = application
+
+    private val _main_text : MutableLiveData<String> = MutableLiveData("Main")
+    val main_text: LiveData<String> get() = _main_text
+
+    fun onClickButton(){
+        // TODO: Click 시 Room에 데이터를 추가해야 함.
+        Toast.makeText(mApplication,"Click!",Toast.LENGTH_SHORT).show()
+    }
 
 }
