cbf01b810acfba138ebc0909b6781693cdaa10e1
hunderboy <masury2011@gmail.com>
2021-10-25 14:54:38 +0900

    getDatabaseCreated 적용하기 전

--- app/src/main/java/com/example/mvvm_room_repository/room/AppDatabase.kt
+++ app/src/main/java/com/example/mvvm_room_repository/room/AppDatabase.kt
@@ -12,14 +12,16 @@ import kotlinx.coroutines.launch
 /**
  * Database class는 추상클래스, Singletone으로 구현되어야 합니다.
  * 일반적으로 Database는 전체 앱에 하나의 인스턴스만 만듭니다.
- ## 본래 database의 schema가 바뀌었을 경우 version을 변경해 주어야 합니다.
+ ## 또한, 본래 database의 schema가 바뀌었을 경우 version을 변경해 주어야 합니다.
  */
 @Database(
     // 만약 하나의 데이터 베이스가 여러 개의 entity를 가져야 한다면,
     // arrayOf() 안에 콤마로 구분해서 entity를 넣어주면 된다.
     // entities = arrayOf(User::class, Student::class)
     entities = [UserEntity::class],
+    // dao나 entity등이 변화했을 경우 version을 2로 바꾸어주고, migration을 진행해야 합니다.
     version = 1,
+    // true로 하면 Room의 schema를 json파일로 생성할 수 있습니다.
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
@@ -47,7 +49,7 @@ abstract class AppDatabase : RoomDatabase() {
                     // RoomDatabaseCallback을 만들어 onCreate()을 override 하여
                     // 데이터베이스가 처음 생성되었을때 할 행동을 코딩할 수 있습니다.
                     .addCallback(AppDatabaseCallback(scope)) // RoomDB 생성될때 초기 데이터 입력을 위해
-                    .fallbackToDestructiveMigration()   //
+                    .fallbackToDestructiveMigration()   // 버전을 바뀌었을때 이전의 버전 데이터 무시하고 시작
                     .build()
 
                 INSTANCE = instance
